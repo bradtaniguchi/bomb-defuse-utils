52a5ed26d333d53f594868cc042f92c123cf2386
bradtaniguchi <bradtaniguchi@gmail.com>
2019-04-01 19:16:21 -0700

    Started adding input using formsModule
    - going to change to be a subject instead.

--- projects/client/src/app/modules/search/search.component.ts
+++ projects/client/src/app/modules/search/search.component.ts
@@ -1,27 +1,45 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Form } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
   template: `
     <main>
-      <div fxLayout="column" fxLayoutAlign="center center">
+      <form novalidate #form="ngForm" (submit)="onSubmit(form)">
         <div fxLayout="column" fxLayoutAlign="center center">
-          <label for="search">Search Utils:</label>
-          <input type="search" id="search" name="search" class="search-box" />
+          <div
+            fxLayout="column"
+            fxLayoutAlign="center center"
+            class="half-width"
+          >
+            <label for="search">Search Utils:</label>
+            <div fxLayout="row" class="full-width">
+              <input
+                type="text"
+                id="search"
+                name="search"
+                [(ngModel)]="search"
+                #_search="ngModel"
+                class="search-box full-width"
+              />
+              <button type="button">
+                Search
+              </button>
+            </div>
+          </div>
         </div>
-      </div>
+      </form>
     </main>
   `,
-  styles: [
-    `
-      .search-box {
-      }
-    `
-  ],
+  styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SearchComponent implements OnInit {
+  public search: string;
   constructor() {}
 
   ngOnInit() {}
+  public onSubmit(form: Form) {
+    console.log('test in form:', form);
+  }
 }
--- projects/client/src/app/modules/search/search.module.ts
+++ projects/client/src/app/modules/search/search.module.ts
@@ -4,9 +4,9 @@ import { CommonModule } from '@angular/common';
 import { SearchRoutingModule } from './search-routing.module';
 import { SearchComponent } from './search.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
-
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [SearchComponent],
-  imports: [CommonModule, SearchRoutingModule, FlexLayoutModule]
+  imports: [CommonModule, SearchRoutingModule, FormsModule, FlexLayoutModule]
 })
 export class SearchModule {}
