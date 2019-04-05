b2048e0f8b86a0288a9aed31e98b59dceb73ba5f
bradtaniguchi <bradtaniguchi@gmail.com>
2019-04-05 13:56:51 -0700

    Added jest testcases for wire service
    - this logic should be tested, as otherwise people will explode!

--- projects/client/src/app/core/services/wires/wires.service.ts
+++ projects/client/src/app/core/services/wires/wires.service.ts
@@ -13,6 +13,7 @@ export class WiresService {
    * @param isSerialOdd if the serial is odd, if not given it is assumed
    * its even.
    * @returns the wire to cut, or null if no solution could be found
+   * THIS IS NOT THE INDEX!
    */
   public getWireToCut(
     wires: WireColor[],
@@ -20,7 +21,7 @@ export class WiresService {
   ): number | null {
     switch (wires.length) {
       case 3:
-        return this.getWireToCutOfThree(wires, isSerialOdd);
+        return this.getWireToCutOfThree(wires);
       case 4:
         return this.getWireToCutOfFour(wires, isSerialOdd);
       case 5:
@@ -38,14 +39,39 @@ export class WiresService {
    * Otherwise, if there is more than one blue wire, cut the last blue wire.
    * Otherwise, cut the last wire.
    */
-  private getWireToCutOfThree(wires, isSerialOdd): number {
-    return 0;
+  private getWireToCutOfThree(wires: WireColor[]): number {
+    const noRedWires = !wires.includes('red');
+    const last = wires.length;
+    if (noRedWires) {
+      return 2;
+    }
+    // logically we can just cut the last one..
+    return last;
   }
 
+  /**
+   *
+   * If there is more than one red wire and the last digit of the serial number is odd, cut the last red wire.
+   * Otherwise, if the last wire is yellow and there are no red wires, cut the first wire.
+   * Otherwise, if there is exactly one blue wire, cut the first wire.
+   * Otherwise, if there is more than one yellow wire, cut the last wire.
+   * Otherwise, cut the second wire.
+   */
   private getWireToCutOfFour(
     wires: WireColor[],
     isSerialOdd?: boolean
   ): number {
+    const redWires = wires.filter(wire => wire === 'red');
+    const multiRedWires = redWires.length > 1;
+    if (multiRedWires && isSerialOdd) {
+      return wires.length;
+    }
+    const noRedWires = redWires.length === 0;
+    const lastIsYellow = wires[wires.length - 1] === 'yellow';
+    if (lastIsYellow && noRedWires) {
+      return 0;
+    }
+    // TODO
     return 0;
   }
 
--- projects/client/src/app/core/services/wires/wires.spec.ts
+++ projects/client/src/app/core/services/wires/wires.spec.ts
@@ -2,7 +2,35 @@ import { WiresService } from './wires.service';
 
 describe('wires', () => {
   const wires = new WiresService();
-  it('If there are no red wires, cut the second wire.', () => {
-    expect(wires.getWireToCut(['blue', 'white', 'yellow'])).toEqual(2);
+  describe('three wires: ', () => {
+    test('If there are no red wires, cut the second wire.', () => {
+      expect(wires.getWireToCut(['blue', 'white', 'yellow'])).toEqual(2);
+    });
+
+    test('if the last wire is white, cut the last wire', () => {
+      expect(wires.getWireToCut(['red', 'blue', 'white'])).toEqual(3);
+    });
+
+    test('if there is more than one blue wire, cut the last blue wire', () => {
+      expect(wires.getWireToCut(['red', 'blue', 'blue'])).toEqual(3);
+    });
+  });
+
+  describe('four wires', () => {
+    test('If there is more than one red wire and the last digit of the serial number is odd, cut the last red wire.', () => {
+      expect(wires.getWireToCut(['red', 'white', 'red', 'red'], true)).toEqual(
+        4
+      );
+    });
+    test('if the last wire is yellow and there are no red wires, cut the first wire.', () => {
+      expect(wires.getWireToCut(['white', 'white', 'white', 'yellow'])).toEqual(
+        0
+      );
+    });
+    test('if there is exactly one blue wire, cut the first wire.', () => {
+      expect(wires.getWireToCut(['blue', 'white', 'yellow', 'black'])).toEqual(
+        0
+      );
+    });
   });
 });
