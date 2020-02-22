5deeefd5836afc1ee4838cb302b22a4dde29ae24
bradtaniguchi <bradtaniguchi@gmail.com>
2020-02-22 08:20:33 -0800

    Added service worker back

--- projects/client/src/app/app.module.ts
+++ projects/client/src/app/app.module.ts
@@ -4,9 +4,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderModule } from './core/header/header.module';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HeaderModule, ServiceWorkerModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HeaderModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production
+    })
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
